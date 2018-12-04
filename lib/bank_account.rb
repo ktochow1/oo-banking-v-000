722cf77c558000833373160937710ae4cdf53bba
ktochow1 <ktochow1@binghamton.edu>
2018-12-04 15:41:25 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,19 @@
+require 'pry'
 class BankAccount
-
+  
+attr_accessor :balance, :status
+attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit(amount)
+   @balance += amount 
+    # new_balance
+    # binding.pry
+  end
+  
 end
