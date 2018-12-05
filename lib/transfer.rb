da65649b015b85315f9378b2e37f4b43ff2c9951
ktochow1 <ktochow1@binghamton.edu>
2018-12-05 02:51:50 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,23 @@
 class Transfer
-  # your code here
+attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+  
+  def execute_transaction
+    receiver.balance #+= 50
+    sender.balance #-= 50
+    self.status=("complete")
+  end
+  
+  
+  
 end
